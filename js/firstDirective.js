3d901c17b0ecb8fc72dbce650517672bb40e2c50
Ryan Hamblin <rhamblin.contractor@lifevantage.com>
2015-05-20 19:24:16 -0600

    mini project finished

---
+++ js/firstDirective.js
@@ -0,0 +1,10 @@
+// angular.module('timeApp', []);
+// .directive('firstDirective', function(){
+// 	return {
+// 		restric: 'A',
+// 		template: '<p>Hello Newmann!</p>',
+// 		link: function(){
+// 			alert('hi!');
+// 		}
+// 	}
+// });
